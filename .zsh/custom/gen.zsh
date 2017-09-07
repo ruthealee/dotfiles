fc42e1233b2469f89924aaccc361da36acb556d5
Ruth Lee <ruth@ruthealee.co.uk>
2017-09-07 08:18:18 +0100

    cleaned up submodule mess

---
+++ .zsh/custom/gen.zsh
@@ -0,0 +1,15 @@
+# Some generic configuration
+
+export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+
---
+++ .zsh/custom/oh-my-zsh.zshrc
@@ -0,0 +1,5 @@
+# oh-my-zsh specific configuration
+
+# plugins here
+
+plugins=(git colored-man-pages git-extra python pip colorize)
